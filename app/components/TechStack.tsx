a7cee61360290ac7001f8ea60f78b92ae12ec0af
Peter Kwamena Essibu <164653045+peterkessibu@users.noreply.github.com>
2024-09-19 07:35:15 +0000

    UI Updated: Techstack

--- app/components/TechStack.tsx
+++ app/components/TechStack.tsx
@@ -9,71 +9,154 @@ const TechStack = () => {
     threshold: 0.2,
   });
 
-  const techStackImages = [
-    '/images/react.png',
-    '/images/nextjs.png',
-    '/images/typescript.png',
-    '/images/tailwindcss.png',
-    '/images/nodejs.png',
-    '/images/firebase.png',
-  ];
-
-  const firstHalf = techStackImages.slice(0, Math.ceil(techStackImages.length / 2));
-  const secondHalf = techStackImages.slice(Math.ceil(techStackImages.length / 2));
+  const techStack = {
+    frameworks: [
+      { src: '/images/Techstack/next.png', alt: 'Next.js' },
+      { src: '/images/Techstack/react.png', alt: 'React.js' },
+    ],
+    libraries: [
+      { src: '/images/Techstack/firebase.png', alt: 'Firebase' },
+      { src: '/images/Techstack/tailwindcss.png', alt: 'Tailwind CSS' },
+    ],
+    languages: [
+      { src: '/images/Techstack/javascript.png', alt: 'JavaScript' },
+      { src: '/images/Techstack/typescript.png', alt: 'TypeScript' },
+      { src: '/images/Techstack/python.png', alt: 'Python' },
+    ],
+    designTools: [
+      { src: '/images/Techstack/figma.png', alt: 'Figma' },
+    ],
+    aiIntegrations: [
+      { src: '/images/Techstack/openrouter.png', alt: 'OpenRouter' },
+      { src: '/images/Techstack/gemini.png', alt: 'Gemini' },
+      { src: '/images/Techstack/openai.png', alt: 'OpenAI' },
+    ],
+  };
 
   return (
     <motion.section
       id="techstack"
-      className="p-10 bg-[#ffffff] w-full max-w-screen"
+      className="p-10 bg-[#e0e7fdea] w-full max-w-screen"
       ref={ref}
       initial={{ opacity: 0 }}
       animate={inView ? { opacity: 1 } : { opacity: 0 }}
-      transition={{ duration: 1.5 }} // Slower animation
+      transition={{ duration: 1.5 }}
     >
       <h2 className="text-3xl font-bold text-center mb-8">Tech Stack</h2>
 
-      <div className="flex justify-center">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 justify-center">
-          {/* Left section */}
-          <motion.div
-            initial={{ opacity: 0, x: -70 }} // Slower animation on both sections
-            animate={inView ? { opacity: 1, x: 0 } : { opacity: 0, x: -70 }}
-            transition={{ duration: 1.5 }}
-            className="flex justify-end space-x-4" // Flexbox layout to align center
-          >
-            {firstHalf.map((src, index) => (
-              <div key={index} className="flex justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {/* Frameworks Section */}
+        <motion.div
+          initial={{ opacity: 0, x: -70 }}
+          animate={inView ? { opacity: 1, x: 0 } : { opacity: 0, x: -70 }}
+          transition={{ duration: 1.5 }}
+          className="flex flex-col items-center"
+        >
+          <h3 className="text-lg font-semibold mb-4">Frameworks</h3>
+          <div className="grid grid-cols-2 gap-6">
+            {techStack.frameworks.map((tech, index) => (
+              <div key={index} className="bg-white shadow-md rounded-lg p-6 flex items-center justify-center transition-transform transform hover:scale-105">
                 <Image
-                  src={src}
-                  alt={`Tech Stack ${index}`}
-                  width={32}
-                  height={32}
-                  className="w-32 h-32 object-contain"
+                  src={tech.src}
+                  alt={tech.alt}
+                  width={64}
+                  height={64}
+                  className="w-18 h-18 object-contain"
                 />
               </div>
             ))}
-          </motion.div>
+          </div>
+        </motion.div>
 
-          {/* Right section */}
-          <motion.div
-            initial={{ opacity: 0, x: 70 }}
-            animate={inView ? { opacity: 1, x: 0 } : { opacity: 0, x: 70 }}
-            transition={{ duration: 1.5 }}
-            className="flex justify-start space-x-4"
-          >
-            {secondHalf.map((src, index) => (
-              <div key={index} className="flex justify-center">
+        {/* Libraries Section */}
+        <motion.div
+          initial={{ opacity: 0, x: 0 }}
+          animate={inView ? { opacity: 1, x: 0 } : { opacity: 0, x: 0 }}
+          transition={{ duration: 1.5 }}
+          className="flex flex-col items-center"
+        >
+          <h3 className="text-lg font-semibold mb-4">Libraries</h3>
+          <div className="grid grid-cols-2 gap-6">
+            {techStack.libraries.map((tech, index) => (
+              <div key={index} className="bg-white shadow-md rounded-lg p-6 flex items-center justify-center transition-transform transform hover:scale-105">
                 <Image
-                  src={src}
-                  alt={`Tech Stack ${index + firstHalf.length}`}
-                  width={32}
-                  height={32}
-                  className="w-32 h-32 object-contain"
+                  src={tech.src}
+                  alt={tech.alt}
+                  width={64}
+                  height={64}
+                  className="w-18 h-18 object-contain"
                 />
               </div>
             ))}
-          </motion.div>
-        </div>
+          </div>
+        </motion.div>
+        {/* AI Integrations Section */}
+        <motion.div
+          initial={{ opacity: 0, x: 70 }}
+          animate={inView ? { opacity: 1, x: 0 } : { opacity: 0, x: 70 }}
+          transition={{ duration: 1.5 }}
+          className="flex flex-col items-center"
+        >
+          <h3 className="text-lg font-semibold mb-4">AI Integrations</h3>
+          <div className="grid grid-cols-2 gap-6">
+            {techStack.aiIntegrations.map((tech, index) => (
+              <div key={index} className="bg-white shadow-md rounded-lg p-6 flex items-center justify-center transition-transform transform hover:scale-105">
+                <Image
+                  src={tech.src}
+                  alt={tech.alt}
+                  width={64}
+                  height={64}
+                  className="w-18 h-18 object-contain"
+                />
+              </div>
+            ))}
+          </div>
+        </motion.div>
+        {/* Languages Section */}
+        <motion.div
+          initial={{ opacity: 0, x: 70 }}
+          animate={inView ? { opacity: 1, x: 0 } : { opacity: 0, x: 70 }}
+          transition={{ duration: 1.5 }}
+          className="flex flex-col items-center"
+        >
+          <h3 className="text-lg font-semibold mb-4">Languages</h3>
+          <div className="grid grid-cols-2 gap-6">
+            {techStack.languages.map((tech, index) => (
+              <div key={index} className="bg-white shadow-md rounded-lg p-6 flex items-center justify-center transition-transform transform hover:scale-105">
+                <Image
+                  src={tech.src}
+                  alt={tech.alt}
+                  width={64}
+                  height={64}
+                  className="w-18 h-18 object-contain"
+                />
+              </div>
+            ))}
+          </div>
+        </motion.div>
+
+        {/* Design Tools Section */}
+        <motion.div
+          initial={{ opacity: 0, x: 70 }}
+          animate={inView ? { opacity: 1, x: 0 } : { opacity: 0, x: 70 }}
+          transition={{ duration: 1.5 }}
+          className="flex flex-col items-center"
+        >
+          <h3 className="text-lg font-semibold mb-4">Design Tools</h3>
+          <div className="grid grid-cols-2 gap-6">
+            {techStack.designTools.map((tech, index) => (
+              <div key={index} className="bg-white shadow-md rounded-lg p-6 flex items-center justify-center transition-transform transform hover:scale-105">
+                <Image
+                  src={tech.src}
+                  alt={tech.alt}
+                  width={64}
+                  height={64}
+                  className="w-18 h-18 object-contain"
+                />
+              </div>
+            ))}
+          </div>
+        </motion.div>
       </div>
     </motion.section>
   );
