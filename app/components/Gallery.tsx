70b9d5495ffb4907a83cd5e582ac6e0842591bcc
Peter Kwamena Essibu <164653045+peterkessibu@users.noreply.github.com>
2024-09-22 21:08:43 +0000

    Aniamtions Added

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const Gallery = () => {
   // Combined images array
@@ -9,19 +10,41 @@ const Gallery = () => {
     "/images/Gallery/me_image_3.jpg",
   ];
 
+  // Animation variants
+  const variants = {
+    hidden: (direction: number) => ({
+      opacity: 0,
+      y: direction > 0 ? 50 : -50,
+    }),
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
   return (
     <div id="gallery" className="p-10 bg-white w-full max-w-screen">
       <h2 className="text-3xl font-bold text-center mb-8">Gallery</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {images.map((src, index) => (
-          <Image
+          <motion.div
             key={index}
-            src={src}
-            alt={`Image ${index + 1}`}
-            width={400} // Set the width
-            height={0} // Remove fixed height
-            className="object-contain w-full h-auto rounded-b-xl" // Maintain the aspect ratio
-          />
+            custom={index % 2 === 0 ? 1 : -1} // Alternate direction
+            initial="hidden"
+            animate="visible"
+            variants={variants}
+          >
+            <Image
+              src={src}
+              alt={`Image ${index + 1}`}
+              width={400} // Set the width
+              height={0} // Remove fixed height
+              className="object-contain w-full h-auto rounded-b-xl" // Maintain the aspect ratio
+            />
+          </motion.div>
         ))}
       </div>
     </div>
