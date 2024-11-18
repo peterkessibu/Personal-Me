e76df7b508dd8f7e6a0e22883b6922a2ad8beb4a
Peter Kwamena Essibu <164653045+peterkessibu@users.noreply.github.com>
2024-11-18 05:07:34 +0000

    Refactor Projects component for improved layout and animation

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -57,98 +57,98 @@ const Projects = () => {
 
   return (
     <motion.section
-      id="projects"
-      className=" bg-white w-full text-[#040413]"
-      ref={ref}
-      initial={{ opacity: 0, y: 50 }}
-      animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
-      transition={{ duration: 0.6, ease: "easeInOut" }}
-    >
-      <motion.h2
-        className="text-5xl font-bold text-center my-12"
-        initial={{ opacity: 0, y: -30 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8, ease: "easeInOut" }}
-      >
-        Projects
-      </motion.h2>
+  id="projects"
+  className="bg-white w-full text-[#040413]"
+  ref={ref}
+  initial={{ opacity: 0, y: 50 }}
+  animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+  transition={{ duration: 0.6, ease: "easeInOut" }}
+>
+  <motion.h2
+    className="text-5xl font-bold text-center my-12"
+    initial={{ opacity: 0, y: -30 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.8, ease: "easeInOut" }}
+  >
+    Projects
+  </motion.h2>
 
+  <motion.div
+    className="grid grid-cols-1 md:grid-cols-2 gap-8 p-4"
+    variants={containerVariants}
+    initial="hidden"
+    animate="visible"
+  >
+    {projects.map((project, index) => (
       <motion.div
-        className="grid grid-cols-1 gap-y-8 p-4"
-        variants={containerVariants}
-        initial="hidden"
-        animate="visible"
+        key={index}
+        className="border p-4 rounded-lg shadow-lg relative overflow-hidden transition-transform transform hover:scale-105"
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 0.5,
+          delay: index * 0.15,
+          ease: "easeInOut",
+        }}
       >
-        {projects.map((project, index) => (
-          <motion.div
-            key={index}
-            className="border p-10 rounded-lg shadow-lg relative overflow-hidden transition-transform transform hover:scale-105"
-            initial={{ opacity: 0, scale: 0.9 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              duration: 0.5,
-              delay: index * 0.15,
-              ease: "easeInOut",
-            }}
-          >
-            <div className="absolute top-0 left-0 w-full p-4 z-20 flex justify-between items-center mb-4">
-              <h3 className="text-xl font-bold">{project.name}</h3>
-              <div className="flex space-x-3">
-                {project.links.demo && (
-                  <Link
-                    href={project.links.demo}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-black hover:text-[#7881f5] transition duration-300"
-                  >
-                    <FaLink className="w-5 h-5" />
-                  </Link>
-                )}
-                {project.links.github && (
-                  <Link
-                    href={project.links.github}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-black hover:text-black transition duration-300"
-                  >
-                    <FaGithub className="w-5 h-5" />
-                  </Link>
-                )}
-                {project.links.youtube && (
-                  <Link
-                    href={project.links.youtube}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-black hover:text-[#ca3939] transition duration-300"
-                  >
-                    <FaYoutube className="w-5 h-5" />
-                  </Link>
-                )}
-              </div>
-            </div>
-
-            <div className="flex flex-col md:flex-row items-center">
-              {/* Image Animation */}
-              <motion.div
-                className="w-full md:w-1/2 mb-4 mt-8 relative aspect-video"
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5, ease: "easeInOut" }}
+        <div className="absolute top-0 left-0 w-full p-4 z-20 flex justify-between items-center mb-4">
+          <h3 className="text-xl font-bold">{project.name}</h3>
+          <div className="flex space-x-3">
+            {project.links.demo && (
+              <Link
+                href={project.links.demo}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-black hover:text-[#7881f5] transition duration-300"
               >
-                <Image
-                  src={project.imgSrc}
-                  alt={project.name}
-                  fill
-                  sizes="(max-width: 768px) 100vw, 50vw"
-                  quality={100}
-                  className="rounded-t-xl p-4 object-contain"
-                />
-              </motion.div>
-            </div>
+                <FaLink className="w-5 h-5" />
+              </Link>
+            )}
+            {project.links.github && (
+              <Link
+                href={project.links.github}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-black hover:text-black transition duration-300"
+              >
+                <FaGithub className="w-5 h-5" />
+              </Link>
+            )}
+            {project.links.youtube && (
+              <Link
+                href={project.links.youtube}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-black hover:text-[#ca3939] transition duration-300"
+              >
+                <FaYoutube className="w-5 h-5" />
+              </Link>
+            )}
+          </div>
+        </div>
+
+        <div className="flex justify-center items-center">
+          {/* Image Animation */}
+          <motion.div
+            className="w-full mt-8 relative aspect-video"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+          >
+            <Image
+              src={project.imgSrc}
+              alt={project.name}
+              fill
+              sizes="(max-width: 768px) 100vw, 50vw"
+              quality={100}
+              className="rounded-lg p-4 object-contain"
+            />
           </motion.div>
-        ))}
+        </div>
       </motion.div>
-    </motion.section>
+    ))}
+  </motion.div>
+</motion.section>
   );
 };
 
